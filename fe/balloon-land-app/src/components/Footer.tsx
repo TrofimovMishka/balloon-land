abbc8afa1ca711922d62d15ee91187d2b8902683
Mykhailo Trofimov <mykhailo.trofimov@gmail.com>
2025-09-28 23:46:08 +0200

    [FE] Adding animation to footer

--- fe/balloon-land-app/src/components/Footer.tsx
+++ fe/balloon-land-app/src/components/Footer.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faSquareFacebook, faFacebookF } from '@fortawesome/free-brands-svg-icons'
-import { faPaperPlane, faSquareEnvelope, faEnvelope,  faSquarePhone, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faPaperPlane, faEnvelope, faSquarePhone, faPhone } from '@fortawesome/free-solid-svg-icons'
 
+const instagramUrl: string = import.meta.env.VITE_INSATGRAM_URL
+const facebookUrl: string = import.meta.env.VITE_FACEBOOK_URL
+const telegramUrl: string = import.meta.env.VITE_TELEGRAM_URL
+const mailToUrl: string = import.meta.env.VITE_MAIL_URL
+const phoneNumber: string = import.meta.env.VITE_PHONE_NUMBER
+
+// TODO: Refactor for phone size
 class Footer extends React.Component {
   render() {
     return (
@@ -11,21 +18,41 @@ class Footer extends React.Component {
         <div className='footer'>
           <div>Write to us and follow us:</div>
           <div className='follow-us'>
-            {/* contact icons */}
-            <div className='instagram'>
-              <FontAwesomeIcon icon={faInstagram} />
+            <div
+              className='social-media'
+              onClick={() => window.open(instagramUrl, "_blank")}
+              style={{ cursor: 'pointer' }}
+
+            >
+              <FontAwesomeIcon icon={faInstagram} size="lg" />
             </div>
-            <div className='fb'>
-              <FontAwesomeIcon icon={faFacebookF} />
+            <div
+              className='social-media'
+              onClick={() => window.open(facebookUrl, "_blank")}
+              style={{ cursor: 'pointer' }}
+
+            >
+              <FontAwesomeIcon icon={faFacebookF} size="lg" />
             </div>
-            <div className='telegram'>
-              <FontAwesomeIcon icon={faPaperPlane} />
+            <div
+              className='social-media'
+              onClick={() => window.open(telegramUrl, "_blank")}
+              style={{ cursor: 'pointer' }}
+
+            >
+              <FontAwesomeIcon icon={faPaperPlane} size="lg" />
             </div>
-            <div className='gmail'>
-              <FontAwesomeIcon icon={faEnvelope} />
+            <div
+              className='social-media'
+              onClick={() => window.open(mailToUrl, "_blank")}
+              style={{ cursor: 'pointer' }}
+
+            >
+              <FontAwesomeIcon icon={faEnvelope} size="lg" />
             </div>
-            <div className='phone'>
-              <FontAwesomeIcon icon={faPhone} />
+            <div className="phone-container">
+              <FontAwesomeIcon icon={faPhone} size="lg" className="phone-icon" />
+              <span className="phone-text">{phoneNumber}</span>
             </div>
           </div>
         </div>
---
+++ fe/balloon-land-app/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+interface ImportMeta {
+  readonly VITE_INSATGRAM_URL: string
+  readonly VITE_FACEBOOK_URL: string
+  readonly VITE_TELEGRAM_URL: string
+  readonly VITE_MAIL_URL: string
+  readonly VITE_PHONE_NUMBER: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMeta}
